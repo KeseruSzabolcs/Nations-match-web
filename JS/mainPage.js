b9195de8f7ca41c6e3b13392d71c749a17c5483b
Keseru Szabolcs <keserusz94@gmail.com>
2020-01-20 13:23:28 +0200

    made method Get one user

--- JS/mainPage.js
+++ JS/mainPage.js
@@ -10,8 +10,17 @@ window.mainPage = {
             console.log(response);
 
             mainPage.display(response.content);
+            //mainPage.displayMainUser(response.content)
         })
 
+    },
+
+    display: function (users) {
+        var usersHtml = "";
+
+        users.forEach(user => usersHtml += mainPage.getUsersHtml(user));
+
+        $(".info #User tbody").html(usersHtml);
     },
 
     getUsersHtml: function (user) {
@@ -30,6 +39,25 @@ window.mainPage = {
                 </tr>`
     },
 
+    getMainUser: function () {
+        $.ajax({
+            url: mainPage.API_BASE_URL,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response.content);
+
+            mainPage.displayMainUser(response.content);
+        })
+
+    },
+
+    displayMainUser: function (user) {
+
+        let usersHtml = mainPage.getMainUserHtml(user[2]);
+
+        $(".info #Personal tbody").html(usersHtml);
+    },
+
     display: function (users) {
         var usersHtml = "";
 
@@ -37,5 +65,23 @@ window.mainPage = {
 
         $(".info #User tbody").html(usersHtml);
     },
+
+    getMainUserHtml: function (user) {
+        return `<tr>
+                    <td><img src=${user.imageUrl} alt="" border=3 height=100 width=100></td>
+                    <td align="center">${user.firstName}<br>${user.lastName}</td>
+                    <td align="center">${user.nationality}</td>
+                    <td>${user.description}</td>
+                    <td align="center">Age:<br>${user.age}<br>Email:<br>${user.email}</td>
+                    <td align="center"><button>Edit</button></td>
+                </tr>
+                <tr>
+                    <td colspan="6"><div class="line"></div></td>
+                </tr>`
+
+
+    },
+
 };
+mainPage.getMainUser();
 mainPage.getUsers();
