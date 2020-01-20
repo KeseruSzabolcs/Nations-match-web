dd9eeb53e031de29c0677b8542e02c5be2f30031
Keseru Szabolcs <keserusz94@gmail.com>
2020-01-20 17:10:39 +0200

    made aboutUs page

--- JS/mainPage.js
+++ JS/mainPage.js
@@ -70,11 +70,15 @@ window.mainPage = {
     getMainUserHtml: function (user) {
         return `<tr>
                     <td><img src=${user.imageUrl} alt="" border=3 height=100 width=100></td>
-                    <td align="center">${user.firstName}<br>${user.lastName}</td>
-                    <td align="center">${user.nationality}</td>
-                    <td>${user.description}</td>
-                    <td align="center">Age:<br>${user.age}<br>Email:<br>${user.email}</td>
-                    <td align="center"><button>Edit</button></td>
+                    <td align="center" contenteditable="true"><div id="firstName">${user.firstName}</div><br><div id="lastName">${user.lastName}</div></td>
+                    <td align="center" contenteditable="true" id="nationality">${user.nationality}</td>
+                    <td contenteditable="true" id="description">${user.description}</td>
+                    <td align="center">Age:<br><div contenteditable="true" id="age">${user.age}</div><br>Email:<br><div contenteditable="true" id="email">${user.email}</div></td>
+                    <td align="center"><a href="#" class="delete-item"><center><i class="far fa-trash-alt" 
+                                            style="font-size: 2.0em"></i></center></a><br>
+                                       <a href="#" class="edit-item" data-id="${user.id}"><i class="fas fa-user-edit" 
+                                            style="font-size: 2.0em"></i></a>
+                    </a></td>
                 </tr>
                 <tr>
                     <td colspan="6"><div class="line"></div></td>
@@ -83,6 +87,49 @@ window.mainPage = {
 
     },
 
+    updateContact: function(id, firstName, lastName, age, description, nationality, imageUrl, email){
+        let requestBody = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            description: description,
+            nationality: nationality,
+            imageUrl:imageUrl,
+            email: email
+        };
+
+        $.ajax({
+            url: mainPage.API_BASE_URL + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            mainPage.getMainUser();
+            mainPage.getUsers()
+        })
+    },
+
+    bindEvents: function () {
+        $(".info #Personal").delegate("edit-item", "change", function (event) {
+            event.preventDefault();
+
+            const personal = {
+                firstName: $('#firstName').val(),
+                lastName: $('#lastName').val(),
+                age: $('#age').val(),
+                description: $('#description').val(),
+                nationality: $('#nationality').val(),
+                imageUrl: $('#imageUrl').val(),
+                email: $('#email').val
+            };
+
+            let id = $(this).data("id");
+
+            mainPage.updateContact(id, personal.firstName, personal.lastName, personal.age,
+                personal.description, personal.nationality, personal.imageUrl, personal.email)
+        });
+    }
 };
 mainPage.getMainUser();
 mainPage.getUsers();
+mainPage.bindEvents();
